c35e35ec3a0e2e6eeefa50e522966eee36d8142f
KaterinaVis <katerina4484@mail.ru>
2022-09-08 22:27:27 +0500

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+
+Console.WriteLine("Введите число от");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число до");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int number =
+SumNumber(n, m);
+// Console.Write(sum);
+
+void SumNumber(int n, int m)
+{
+  if (count > n) return;
+  SumNumber(n, count + 1);
+  Console.Write(count + ",");
+}
