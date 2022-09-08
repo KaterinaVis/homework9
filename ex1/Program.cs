16c512f91ce3be111f32049040c2f0d1536f06b8
KaterinaVis <katerina4484@mail.ru>
2022-09-08 21:33:19 +0500

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Показать натуральные числа от N до 1, N задано (с помощью рекурсии)
+
+// Console.WriteLine("Введите натуральное число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+// Console.Write(1);
+//  Number (n, count);
+
+// void Number (int n, int count)
+// {
+//     if (count>n) return;
+//     Number(count-1, n);
+//     Console.WriteLine(count+ ",");
+   
+// }
+
+Console.WriteLine("Введите натуральное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int num, int a)
+{
+  if (a > num) return;
+  PrintNumber(num, a + 1);
+  Console.Write(a + ",");
+}
+
+
+
+
